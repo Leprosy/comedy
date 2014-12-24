18330604bfd4a3a98c98b284cbf233c82dd68a6f
Miguel Rojas <miguel_rojas@mentor.com>
2014-12-24 12:06:24 -0300

    Cache

--- list.php
+++ list.php
@@ -1,3 +1,22 @@
+<?php
+$path = __DIR__ . '/scripts/';
+$list = array();
+
+if ($handle = opendir($path)) {
+    /* This is the correct way to loop over the directory. */
+    while (false !== ($entry = readdir($handle))) {
+        if ($entry!='.' && $entry!='..') {
+            $content = json_decode(file_get_contents($path . $entry));
+            $content->date = date('Y-m-d H:i:s', filectime($path.$entry));
+            $content->link = $entry;
+            $list[] = $content;
+        }
+    }
+
+    closedir($handle);
+}
+
+?>
 <html>
     <head>        
         <meta charset="utf-8" />
@@ -10,36 +29,29 @@
     </head>
 
     <body>
-        <div class="row">
-            <div class="small-12 column">
-    
-            <?php
-            $path = __DIR__ . '/scripts/';
-            
-            if ($handle = opendir($path)) {
-                /* This is the correct way to loop over the directory. */
-                while (false !== ($entry = readdir($handle))) {
-                	if ($entry!='.' && $entry!='..') {
-                	    $content = file_get_contents($path . $entry);
-                        $date = date('Y-m-d H:i:s', filectime($path.$entry));
-                		?>
-    		            <div class="row">
-                            <div class="small-8 column">
-                		    <?php echo json_decode($content)->title ?> => <a href="<?php echo $entry ?>">comedia/<?php echo $entry ?></a>
-                		    </div>
-                		    <div class="small-4 column">
-                		    <?php echo $date ?>
-                		    </div>
-            		    </div>
-            		    <?php
-                	}
-                }
-    
-                closedir($handle);
-            }
-            ?>
+        <?php 
+        foreach ($list as $item) {
+        ?>
+            <div class="row">
+              <div class="large-12 columns">
+                <div class="panel clearfix">
+                  <h4><a href="<?php echo $item->link ?>"><?php echo $item->title ?></a></h4>
+
+                  <div class="large-2 columns">
+                    <img class="th" src="upimg/<?php echo $item->content[0]->img ?>" width="100" height="100" />
+                  </div>
+
+                  <div class="large-10 columns">
+                    <p>"<?php echo $item->content[0]->caption ?>"</p>
+                    <small>Creado en: <?php echo $item->date ?></small>
+                  </div>
+
+                </div>
+              </div>
             </div>
-        </div>
+        <?php 
+        }
+        ?>
 
         <script src="js/vendor/jquery.js"></script>
         <script src="js/foundation.min.js"></script>
